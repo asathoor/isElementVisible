4cd1d76b9833edd2420e79d924664d0cd9c68508
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2018-09-02 19:19:36 +0200

    tidy code

--- js/audio.js
+++ js/audio.js
@@ -2,24 +2,22 @@
  * fil: audio.js
  * eksempel fra Bror Arnfast
  **/
+window.addEventListener("scroll", function() {
 
- window.addEventListener("scroll", function(){
-          
-    var mySound = document.getElementById("myaudio"); // select audio
-    var lyd = document.getElementById("lyd");
-    console.log( "Er div #lyd synlig? " + elFllVsbl( lyd ) );
-    
-    if (elFllVsbl ( lyd )){ 
-        if (!(mySound.curentTime > 0)){
-            mySound.play(); // play audio
-        }
-    } else {
-        mySound.pause();
-        mySound.currentTime = 0; // rewind the sound
+  var mySound = document.getElementById("myaudio"); // select audio id
+  var lyd = document.getElementById("lyd");
+  console.log("Er div #lyd synlig? " + elFllVsbl(lyd));
+
+  if (elFllVsbl(lyd)) {
+    if (!(mySound.curentTime > 0)) {
+      mySound.play(); // play audio
     }
- }
-)
+  } else {
+    mySound.pause();
+    mySound.currentTime = 0; // rewind the sound
+  }
+})
 
-function elFllVsbl(el){
-    return ( el.getBoundingClientRect().top>=0 &&  el.getBoundingClientRect().bottom<window.innerHeight);
+function elFllVsbl(el) {
+  return (el.getBoundingClientRect().top >= 0 && el.getBoundingClientRect().bottom < window.innerHeight);
 }
--- js/youtube.js
+++ js/youtube.js
@@ -12,55 +12,55 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 // Replace the 'ytplayer' element with an <iframe> and
 // YouTube player after the API code downloads.
 var player;
+
 function onYouTubePlayerAPIReady() {
-player = new YT.Player('ytplayer', {
-  height: '360',
-  width: '640',
-  videoId: '1o645n7OeeA',
+  player = new YT.Player('ytplayer', {
+    height: '360',
+    width: '640',
+    videoId: '1o645n7OeeA',
     events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
-      }
-})
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  })
 }
 
-/* Player kontrolleres af funktioner */    
-function onPlayerReady( event ) {
-    console.log("player is ready");
-    event.target.pauseVideo();
-  }
+/* Player kontrolleres af funktioner */
+function onPlayerReady(event) {
+  console.log("player is ready");
+  event.target.pauseVideo();
+}
 
-function playItSam( event ) {
-    console.log("player is ready");
-    event.target.playVideo(); // event from player
-  }
+function playItSam(event) {
+  console.log("player is ready");
+  event.target.playVideo(); // event from player
+}
 
-function onPlayerStateChange(){
-      console.log("player state change");
+function onPlayerStateChange() {
+  console.log("player state change");
 
-      if (event.data == YT.PlayerState.PLAYING && !done) {
-      //setTimeout(stopVideo, 6000);
-      done = true;
-    }
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    //setTimeout(stopVideo, 6000);
+    done = true;
   }
+}
 
 /* is element visible? if ok playVideo() */
-var elementet = document.getElementById("synlig");    
+var elementet = document.getElementById("synlig");
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function() {
 
-        if ( elFllVsbl( elementet ) ){ 
-            console.log("play video");  
-            player.playVideo(); // with player you can manipulate the video
-        } else {
-            console.log("pause video")
-            player.pauseVideo();
-        }
-    }
-); // end eventlistener
+  if (elFllVsbl(elementet)) {
+    console.log("play video");
+    player.playVideo(); // with player you can manipulate the video
+  } else {
+    console.log("pause video")
+    player.pauseVideo();
+  }
+}); // end eventlistener
 
-function elFllVsbl(el){
-    return ( el.getBoundingClientRect().top>=0 &&  el.getBoundingClientRect().bottom<window.innerHeight);
+function elFllVsbl(el) {
+  return (el.getBoundingClientRect().top >= 0 && el.getBoundingClientRect().bottom < window.innerHeight);
 }
 
 /**
