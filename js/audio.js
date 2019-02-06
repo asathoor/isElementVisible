e1b2ccc431df08faae1468b6a95b36f7650820f8
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2019-02-06 20:15:45 +0100

    comments
    more comments  ...

--- js/audio.js
+++ js/audio.js
@@ -2,12 +2,13 @@
  * fil: audio.js
  * purpose: play audio when the containing div is visible
  **/
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function() { // detect scroll event
 
   var mySound = document.getElementById("myaudio"); // select audio id
   var lyd = document.getElementById("lyd");
-  console.log("Er div #lyd synlig? " + elFllVsbl(lyd));
+  console.log("Er div #lyd synlig? " + elFllVsbl(lyd)); // test
 
+// Below: start and stop audio 
   if (elFllVsbl(lyd)) { // if the #lyd div is visible then
     if (!(mySound.curentTime > 0)) {
       mySound.play(); // play audio
