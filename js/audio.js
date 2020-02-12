613032ee2e8e340f99ef30ea371a665f332c76e7
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2020-02-12 10:18:08 +0100

    audio and video

--- js/audio.js
+++ js/audio.js
@@ -2,26 +2,45 @@
  * fil: audio.js
  * purpose: play audio when the containing div is visible
  **/
-window.addEventListener("scroll", function() { // detect scroll event
+window.addEventListener("scroll", function () { // detect scroll event
 
-  let mySound = document.getElementById("myaudio"); // select audio id
-  let lyd = document.getElementById("lyd");
-  console.log("Is the #lyd visible or not? " + elFllVsbl(lyd)); // test
+	let mySound = document.getElementById("myaudio"); // select audio id
+	let lyd = document.getElementById("lyd");
+	console.log("Is the #lyd visible? " + elFllVsbl(lyd)); // test
 
-// Below: start and stop audio 
-  if (elFllVsbl(lyd)) { // if the #lyd div is visible then
-    if (!(mySound.curentTime > 0)) {
-      mySound.play(); // play audio
-    }
-  } else {
-    mySound.pause();
-    mySound.currentTime = 0; // rewind the sound
-  }
-})
+	// Below: start and stop audio 
+	if (elFllVsbl(lyd)) { // if the #lyd div is visible then
+		if (!(mySound.curentTime > 0)) {
+			mySound.play(); // play audio
+		}
+	} else {
+		mySound.pause();
+		mySound.currentTime = 0; // rewind the sound
+	}
+
+	// audio
+	let iSee = document.getElementById('iSee');
+	let myVideo = document.getElementById('myVideo');
+
+
+	console.log('Is the video #iSee visible? ' + elFllVsbl(iSee));
+// video visible test
+	if (elFllVsbl( iSee )) { // if the #lyd div is visible then
+		if (!(myVideo.curentTime > 0)) {
+			myVideo.play(); // play video
+		}
+	} else {
+		myVideo.pause();
+		myVideo.currentTime = 0; // rewind the video
+	}
+
+	// from her if ... play
+
+}); // end eventlistener
 
 // this function will check whether a tag is visible
 function elFllVsbl(el) {
-  return (el.getBoundingClientRect().top >= 0 && el.getBoundingClientRect().bottom < window.innerHeight);
+	return (el.getBoundingClientRect().top >= 0 && el.getBoundingClientRect().bottom < window.innerHeight);
 }
 
 /*
